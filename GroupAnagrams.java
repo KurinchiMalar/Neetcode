0fe0fba273953946c155ee94f98bb603eb2133b9
KurinchiMalar <kurn@paypal.com>
2022-05-19 17:00:49 +0530

    GroupAnagrams - Efficient

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -25,10 +25,32 @@ class GroupAnagrams {
         return true;
     }
     /*
+     wont work for the following two reasons
+     1. ascii sum will be same for ad , bc
+     2. duh , ill ascii sum same as string can have repeated characters it wont work.
+     */
+    public static boolean isValidAnagramAsciiSum(String s, String t){
+        if(s==null || t==null){
+            return false;
+        }
+        int sourceSum = 0;
+        int targetSum = 0;
+        for(Character ch:s.toCharArray()){
+            sourceSum += ch;
+        }
+        for(Character ch:t.toCharArray()){
+            targetSum += ch;
+        }
+        if(sourceSum == targetSum){
+            return true;
+        }
+        return false;
+    }
+    /*
   Time : O(n) - Single parse of original array
   Space : O(n)
    */
-    public static List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagramsNaive(String[] strs) {
         ArrayList<List<String>> resultList = new ArrayList<List<String>>();
 
         if(strs == null){
@@ -63,10 +85,35 @@ class GroupAnagrams {
         }
         return resultList;
     }
-    public static void main(String[] args){
-        String[] strs={"eat","tea","tan","ate","nat","bat"};
-        List<List<String>> resultList = groupAnagrams(strs);
+    /*
+    TimeComplexity : O(n)
+    SpaceComplexity :O(n)
+           worst case when there are no anagram pairs...
+           all strings will have corresponding  keys in map.
+     */
+    public static List<List<String>> groupAnagramsEfficient(String[] strs) {
+        if(strs==null || strs.length==0){
+            return new ArrayList<>();
+        }
+        HashMap<String,ArrayList<String>> hmap = new HashMap<>();
+        for(String s:strs){
+            char[] freq = new char[26];
+            for(Character ch:s.toCharArray()){
+                freq[ch-'a']++;
+            }
+            String curStringCharacterFreqValue = String.valueOf(freq);
+            if(!hmap.containsKey(curStringCharacterFreqValue)){
+                hmap.put(curStringCharacterFreqValue,new ArrayList<>());
+            }
+            hmap.get(curStringCharacterFreqValue).add(s);
+        }
+        return new ArrayList<>(hmap.values());
+    }
 
+        public static void main(String[] args){
+        String[] strs={"eat","tea","tan","ate","nat","bat"};
+        //List<List<String>> resultList = groupAnagrams(strs);
+            List<List<String>> resultList = groupAnagramsEfficient(strs);
         System.out.println(resultList);
     }
 }
