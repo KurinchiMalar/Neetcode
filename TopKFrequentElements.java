be508e7ab3a3d214d008b53c9b93f8065809d964
KurinchiMalar <kurn@paypal.com>
2022-05-21 17:59:31 +0530

    TopKFrequentElements - my solution submitted and success.

--- TopKFrequentElements.java
+++ TopKFrequentElements.java
@@ -22,6 +22,39 @@ It is guaranteed that the answer is unique.
 
 Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
  */
-class TopKFrequentElements{
+/*
+	Time Complexity :O(n)+O(k) // input parse + k times parse on freq hash map
+	Space Complexity :O(n) // worst case no repeated elements
+ */
+import java.util.*;
+import static java.util.stream.Collectors.*;
 
+class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums==null || nums.length==0){
+            return new int[0];
+        }
+        if(nums.length==1){
+            return new int[]{nums[0]};
+        }
+        HashMap<Integer,Integer> numsFrequencyMap = new HashMap<>();
+        for(int num:nums){
+            numsFrequencyMap.put(num,numsFrequencyMap.getOrDefault(num,0)+1);
+        }
+        HashMap<Integer,Integer> numsFrequencySortedDescMap = numsFrequencyMap
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(
+                        toMap(Map.Entry::getKey,Map.Entry::getValue,(e1,e2) -> e2,
+                                LinkedHashMap::new));
+
+        //int[] valuesArray = (int[])numsFrequencySortedDescMap.values().toArray();
+        ArrayList<Integer> keysList = new ArrayList<>(numsFrequencySortedDescMap.keySet());
+        int[] resultAr = new int[k];
+        for(int i=0;i < k;i++){
+            resultAr[i] = keysList.get(i);
+        }
+        return resultAr;
+    }
 }
