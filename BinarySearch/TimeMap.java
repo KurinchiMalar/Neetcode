9c8548fdd003da9df56e7dae7658fc6fddf8c9ae
KurinchiMalar <kurinchimalar.n@gmail.com>
2022-11-01 22:30:57 +0530

    TimeMap -  HashMap<String, List<Data>> map - Completed

--- BinarySearch/TimeMap.java
+++ BinarySearch/TimeMap.java
@@ -1,68 +1,107 @@
+
+/*
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+
+
+Example 1:
+
+Input
+["TimeMap", "set", "get", "get", "set", "get", "get"]
+[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+Output
+[null, null, "bar", "bar", null, "bar2", "bar2"]
+
+Explanation
+TimeMap timeMap = new TimeMap();
+timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+timeMap.get("foo", 1);         // return "bar"
+timeMap.get("foo", 3);         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+timeMap.get("foo", 4);         // return "bar2"
+timeMap.get("foo", 5);         // return "bar2"
+
+
+Constraints:
+
+1 <= key.length, value.length <= 100
+key and value consist of lowercase English letters and digits.
+1 <= timestamp <= 107
+All the timestamps timestamp of set are strictly increasing.
+At most 2 * 105 calls will be made to set and get.
+ */
+
+/*
+All the timestamps timestamp of set are strictly increasing.
+ */
 package BinarySearch;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+
+import java.sql.Time;
+import java.util.*;
 
 
-class TimeMapDS {
-    HashMap<String, LinkedHashMap<Integer,String>> dmap;
-
-    public TimeMapDS() {
-        dmap = new HashMap<String,LinkedHashMap<Integer,String>>();
-    }
-
-    public void set(String key, String value, int timestamp) {
-
-        if(dmap.containsKey(key)){
-            dmap.get(key).put(timestamp,value);
-
-        }else{
-            LinkedHashMap<Integer,String> tsmap = new LinkedHashMap<Integer,String>();
-            tsmap.put(timestamp,value);
-            dmap.put(key,tsmap);
-        }
-
-    }
-
-    public String get(String key, int timestamp) {
-
-        if(!dmap.containsKey(key)){
-            return "";
-        }else{
-            LinkedHashMap<Integer,String> tsmap = dmap.get(key);
-            if(tsmap.containsKey(timestamp)){
-                return tsmap.get(timestamp);
-            }else{
-                // we need to find the nearest maximum timestamp available in tsmap to given timestamp
-                Integer[] nums = tsmap.keySet().toArray(new Integer[tsmap.size()]);
-
-                int left = 0;
-                int right = tsmap.size()-1;
-                while(left <= right){
-                    int mid = left + ((right-left)>>1);
-                    if(timestamp < nums[mid]){
-                        right = mid - 1;
-                    }else{
-                        left = mid + 1;
-                    }
-                }
-                if(right < 0) {
-                    return (timestamp < nums[left])?"":tsmap.get(nums[left]);    // if requested value is less than the most minimum in array return ""
-                }else if(left > tsmap.size()-1){
-                    return (timestamp < nums[right])?"":tsmap.get(nums[right]);
-                }
-                // considering (right, left) adjacent elements .. one with mimimum difference with timestamp is the nearest max.
-                return (nums[left] - timestamp) < (timestamp - nums[right]) ? tsmap.get(nums[left]) : tsmap.get(nums[right]);
-            }
-        }
-
+class Data{
+    int timeStamp;
+    String value;
+    public Data(int timeStamp,String value){
+        this.timeStamp = timeStamp;
+        this.value = value;
     }
 }
 
 public class TimeMap{
 
+    HashMap<String, List<Data>> map;
+    public TimeMap(){
+        map = new HashMap<String, List<Data>>();
+    }
+    public void set(String key, String value, int timestamp){
+        if(!map.containsKey(key)){
+            map.put(key,new ArrayList<Data>());
+        }
+        map.get(key).add(new Data(timestamp,value));
+    }
+
+    public String get(String key,int timestamp){
+        if(!map.containsKey(key)){
+            return "";
+        }
+        return binarySearch(map.get(key),timestamp);
+    }
+
+    public String binarySearch(List<Data> dataList,int timeStamp){
+
+        int low = 0;
+        int high = dataList.size()-1;
+        while(low < high){
+            int mid = low + ((high - low)>>1);
+
+            if(dataList.get(mid).timeStamp==timeStamp){
+                return dataList.get(mid).value;
+            }
+
+            if(dataList.get(mid).timeStamp < timeStamp){ // go to right half
+
+                if(dataList.get(mid+1).timeStamp > timeStamp){
+                    return dataList.get(mid).value;
+                }
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return ((timeStamp > dataList.get(low).timeStamp) ? dataList.get(low).value:"");  // low = high = 10 , timestamp = 5
+    }
+
+
+
     public static void main(String[] args){
-        TimeMapDS tob = new TimeMapDS();
+        TimeMap tob = new TimeMap();
         tob.set("love","high",10);
         tob.set("love","low",20);
         System.out.println(tob.get("love",5));
