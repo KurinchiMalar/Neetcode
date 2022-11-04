354ca6f8a1f3761a7463ccf9efbb83b01e6a5dd8
KurinchiMalar <kurinchimalar.n@gmail.com>
2022-11-04 12:54:46 +0530

    CopyListWithRandomPointer - O(1) space implementation and O(n) space using hashmap

--- LinkedList/CopyListWithRandomPointer.java
+++ LinkedList/CopyListWithRandomPointer.java
@@ -18,5 +18,94 @@ https://leetcode.com/problems/copy-list-with-random-pointer/description/
  */
 package LinkedList;
 
+import java.util.HashMap;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
 public class CopyListWithRandomPointer {
+
+    /*
+    TC : O(n)
+    SC : O(1)
+     */
+    public Node copyRandomListWithoutExtraSpace(Node head) {
+
+        if(head == null){
+            return null;
+        }
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        Node current = head;
+        while(current != null){
+            Node temp = current.next;
+            current.next = new Node(current.val);
+            current.next.next = temp;
+            current = temp;
+        }
+        // Second round: assign random pointers for the copy nodes.
+        current = head;
+        while(current != null){
+            if(current.random == null){
+                current.next.random = null;
+            }else{
+                current.next.random = current.random.next;
+            }
+            current = current.next.next;
+        }
+        // Third round: restore the original list, and extract the copy list.
+        current = head;
+        Node copyHead = head.next;
+        Node copycurrent = copyHead;
+
+        while(copycurrent.next != null){
+            current.next = current.next.next;
+            current = current.next;
+
+            copycurrent.next = copycurrent.next.next;
+            copycurrent = copycurrent.next;
+        }
+        //current and copycurrent at respective last nodes
+        // last node in orig list next pointer should be updated
+        current = current.next.next;
+        return copyHead;
+    }
+    /*
+    TC : O(n)
+    SC : O(n)
+     */
+    public Node copyRandomListWithExtraSpace(Node head) {
+
+        if(head == null){
+            return null;
+        }
+        Node current = head;
+        HashMap<Node,Node> oldToCopyMap = new HashMap<>();
+        oldToCopyMap.put(null,null); // when random points in orig points to null node.
+        // create oldToCopyMap with oldnode to newnode mapping - pass 1
+        while(current != null){
+            oldToCopyMap.put(current,new Node(current.val));
+            current = current.next;
+        }
+
+        current = head;
+        //
+        while(current != null){
+            Node copy = oldToCopyMap.get(current);
+            copy.next = oldToCopyMap.get(current.next);
+            copy.random = oldToCopyMap.get(current.random);
+            current = current.next;
+        }
+        return oldToCopyMap.get(head);
+    }
+
 }
