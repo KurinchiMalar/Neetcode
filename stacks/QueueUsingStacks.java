fbbc438ef90fe7e7990ebf0030a5e075710c5ea5
KurinchiMalar <kurinchimalar.n@gmail.com>
2025-01-21 13:26:56 +0000

    QueueUsingStacks

---
+++ stacks/QueueUsingStacks.java
@@ -0,0 +1,58 @@
+package stacks;
+/*
+https://leetcode.com/problems/implement-queue-using-stacks/description/
+ */
+
+import java.util.Stack;
+
+class MyQueue {
+    Stack s1;
+    Stack s2;
+
+    public MyQueue() {
+        s1 = new Stack();
+        s2 = new Stack();
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return (int)s2.pop();
+    }
+
+    public int peek() {
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return (int)s2.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+
+    }
+}
+
+public class QueueUsingStacks {
+    public static void main(String[] args) {
+
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        System.out.println(myQueue.peek()); // return 1
+        System.out.println(myQueue.pop()); // return 1, queue is [2]
+        System.out.println(myQueue.empty()); // return false
+
+    }
+
+
+}
